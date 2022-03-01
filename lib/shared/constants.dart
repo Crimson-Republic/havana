8b09eca1644859e4c2f678ff40244b5eb8603dc7
Othniel Ussher <othnielussher16@gmail.com>
2022-03-01 03:38:55 +0000

    1️⃣ Complete UI 01

---
+++ lib/shared/constants.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  static const Color primary = Color(0xFF14BD74);
+  static const Color black = Color(0xFF050D2E);
+  static const Color grey = Color(0xFF878787);
+}
+
+class Images {
+  static const String walk = 'assets/images/walk.svg';
+}
+
+class Fonts {
+  static const String lora = 'Lora';
+  static const String openSans = 'OpenSans';
+}
+
+const String word =
+    'Keep this Book of the Law always on your lips; meditate on it day and night, so that you may be careful to do everything written in it. Then you will be prosperous and successful.';
