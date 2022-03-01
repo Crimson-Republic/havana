a03163eb0588c3f90a331cc860b1c50103a33180
Othniel Ussher <othnielussher16@gmail.com>
2022-03-01 03:38:58 +0000

    1️⃣ Complete UI 01

---
+++ lib/shared/exports.dart
@@ -0,0 +1,10 @@
+export 'package:flutter_svg/svg.dart';
+
+export 'package:havana/views/home.dart';
+export 'package:havana/widgets/heading.dart';
+export 'package:havana/shared/constants.dart';
+export 'package:havana/widgets/green_dot.dart';
+export 'package:havana/widgets/skip_button.dart';
+export 'package:havana/widgets/sub_heading.dart';
+export 'package:havana/widgets/dots_indicator.dart';
+export 'package:havana/widgets/get_started_button.dart';
